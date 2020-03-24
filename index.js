50c4708a47a6db931ba9ac5029b488fe3e858023
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-24 16:34:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function breakOut (array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] == stopValue) {
-      break
+    if (array[i] !== stopValue) {
+      array[i] = changeValue
     }
-    array[i] = changeValue;
+    break
   }
 
 }
